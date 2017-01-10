8c56bf2c6f9913775becbbba25129e51795951e5
joeduffy <joeduffy@acm.org>
2017-01-10 14:09:36 -0800

    Add contract.requires functions

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -5,17 +5,10 @@
 import * as process from "process";
 import * as util from "util";
 
-const failCode = -227;
-const failMsg: string = "A failure has occurred";
+
+// Assertions:
+
 const assertMsg: string = "An assertion failure has occurred";
-
-export function fail(): never {
-    return failfast(failMsg);
-}
-
-export function failf(msg: string, ...args: any[]): never {
-    return failfast(`${failMsg}: ${util.format(msg, ...args)}`);
-}
 
 export function assert(b: boolean): void {
     if (!b) {
@@ -29,10 +22,40 @@ export function assertf(b: boolean, msg: string, ...args: any[]): void {
     }
 }
 
+// Fail-fast:
+
+const failMsg: string = "A failure has occurred";
+
+export function fail(): never {
+    return failfast(failMsg);
+}
+
+export function failf(msg: string, ...args: any[]): never {
+    return failfast(`${failMsg}: ${util.format(msg, ...args)}`);
+}
+
+const failCode = -227;
+
 function failfast(msg: string): never {
     Error.stackTraceLimit = 1000;
     console.error(new Error(msg).stack);
     process.exit(failCode);
     while (true) {} // this will never be reached, thanks to process.exit, but makes TSC happy.
+}
+
+// Requires preconditions:
+
+const requiresMsg: string = "'s precondition has bee violated";
+
+export function requires(b: boolean, arg: string): void {
+    if (!b) {
+        return failfast(`${arg}${requiresMsg}`);
+    }
+}
+
+export function requiresf(b: boolean, arg: string, msg: string, ...args: any[]): void {
+    if (!b) {
+        return failfast(`${arg}${requiresMsg}: ${util.format(msg, ...args)}`);
+    }
 }
 
